417d7da5582dfbd22ea1973248d4e96ed48fd4f6
SShanu16 <36079628+SShanu16@users.noreply.github.com>
2018-10-06 12:22:45 +0530

    Modification for heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
+
+const port=process.env.PORT||3000;
 //To create app
 var app=express();
 
@@ -59,4 +61,6 @@ app.get('/about',(req,res)=>{
   })
 });
 //Binds the application to a port on our machine,untill we bind our app it won't start
-app.listen(3000);
+app.listen(port,()=>{
+  console.log(`server starting on port ${port}` )
+});
